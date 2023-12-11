f97ad9dedb7846441419353d7f470a3e9be111ee
zemCool <73570667+zemCool@users.noreply.github.com>
2023-12-11 21:18:09 +0600

    Add files via upload

---
+++ TicTacToe Game/TicTacToe Game/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace TicTacToe_Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            label1 = new Label();
+            label2 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            button10 = new Button();
+            CPUTimer = new System.Windows.Forms.Timer(components);
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label1.ForeColor = Color.OliveDrab;
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(125, 28);
+            label1.TabIndex = 0;
+            label1.Text = "Player wins:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label2.ForeColor = Color.Brown;
+            label2.Location = new Point(299, 9);
+            label2.Name = "label2";
+            label2.Size = new Size(104, 28);
+            label2.TabIndex = 1;
+            label2.Text = "CPU wins:";
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.Control;
+            button1.Location = new Point(37, 48);
+            button1.Name = "button1";
+            button1.Size = new Size(125, 108);
+            button1.TabIndex = 2;
+            button1.Text = "?";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += PlayerClickButton;
+            // 
+            // button2
+            // 
+            button2.BackColor = SystemColors.Control;
+            button2.Location = new Point(168, 48);
+            button2.Name = "button2";
+            button2.Size = new Size(125, 108);
+            button2.TabIndex = 3;
+            button2.Text = "?";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += PlayerClickButton;
+            // 
+            // button3
+            // 
+            button3.BackColor = SystemColors.Control;
+            button3.Location = new Point(299, 48);
+            button3.Name = "button3";
+            button3.Size = new Size(125, 108);
+            button3.TabIndex = 4;
+            button3.Text = "?";
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += PlayerClickButton;
+            // 
+            // button4
+            // 
+            button4.BackColor = SystemColors.Control;
+            button4.Location = new Point(37, 162);
+            button4.Name = "button4";
+            button4.Size = new Size(125, 108);
+            button4.TabIndex = 2;
+            button4.Text = "?";
+            button4.UseVisualStyleBackColor = false;
+            button4.Click += PlayerClickButton;
+            // 
+            // button5
+            // 
+            button5.BackColor = SystemColors.Control;
+            button5.Location = new Point(168, 162);
+            button5.Name = "button5";
+            button5.Size = new Size(125, 108);
+            button5.TabIndex = 3;
+            button5.Text = "?";
+            button5.UseVisualStyleBackColor = false;
+            button5.Click += PlayerClickButton;
+            // 
+            // button6
+            // 
+            button6.BackColor = SystemColors.Control;
+            button6.Location = new Point(299, 162);
+            button6.Name = "button6";
+            button6.Size = new Size(125, 108);
+            button6.TabIndex = 4;
+            button6.Text = "?";
+            button6.UseVisualStyleBackColor = false;
+            button6.Click += PlayerClickButton;
+            // 
+            // button7
+            // 
+            button7.BackColor = SystemColors.Control;
+            button7.Location = new Point(37, 276);
+            button7.Name = "button7";
+            button7.Size = new Size(125, 108);
+            button7.TabIndex = 2;
+            button7.Text = "?";
+            button7.UseVisualStyleBackColor = false;
+            button7.Click += PlayerClickButton;
+            // 
+            // button8
+            // 
+            button8.BackColor = SystemColors.Control;
+            button8.Location = new Point(168, 276);
+            button8.Name = "button8";
+            button8.Size = new Size(125, 108);
+            button8.TabIndex = 3;
+            button8.Text = "?";
+            button8.UseVisualStyleBackColor = false;
+            button8.Click += PlayerClickButton;
+            // 
+            // button9
+            // 
+            button9.BackColor = SystemColors.Control;
+            button9.Location = new Point(299, 276);
+            button9.Name = "button9";
+            button9.Size = new Size(125, 108);
+            button9.TabIndex = 4;
+            button9.Text = "?";
+            button9.UseVisualStyleBackColor = false;
+            button9.Click += PlayerClickButton;
+            // 
+            // button10
+            // 
+            button10.Location = new Point(168, 398);
+            button10.Name = "button10";
+            button10.Size = new Size(125, 43);
+            button10.TabIndex = 5;
+            button10.Text = "Restart Game";
+            button10.UseVisualStyleBackColor = true;
+            button10.Click += RestartGame;
+            // 
+            // CPUTimer
+            // 
+            CPUTimer.Interval = 1000;
+            CPUTimer.Tick += CPUmove;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(482, 453);
+            Controls.Add(button10);
+            Controls.Add(button9);
+            Controls.Add(button6);
+            Controls.Add(button3);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "TicTacToe Game";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button button10;
+        private System.Windows.Forms.Timer CPUTimer;
+    }
+}
